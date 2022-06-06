612dbfd5ff8832804c0c3ccbcc48befca80297be
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-06-06 11:58:33 -0400

    cart close

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,18 +3,18 @@ import React, { Fragment } from "react";
 import ReactDom from "react-dom";
 
 const Backdrop = (props) => {
-    return <div className={classes.backdrop} />
+    return <div className={classes.backdrop} onClick={props.onClose} />
 };
 const ModalOverlay = (props) => {
     return <div className={classes.modal}>
         <div className={classes.content}>{props.children}</div>
     </div>
-}
+};
 
 const portlElement = document.getElementById("overlays")
 const Modal = (props) => {
     return <Fragment>
-        {ReactDom.createPortal(<Backdrop />, portlElement)}
+        {ReactDom.createPortal(<Backdrop onClose={props.onClose} />, portlElement)}
 
         {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portlElement)}
     </Fragment>
